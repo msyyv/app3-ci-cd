3c2f3646028d0c55b2190126831b4fba61acebad
msvvy <rakeshkr.kumar290@gmail.com>
2022-03-30 11:03:05 +0530

    first commit

---
+++ rakesh2.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app=Flask(__name__)
+
+@app.route("/rk")
+def rakesh2():
+    return "Hello Rakesh and jeet welcome to emicon"
+
+
+@app.route("/")
+def rakesh1():
+    return "Hello Rakesh and jeet welcome to emicon and resourcebusy"
+    
+if __name__=='__main__':
+    app.run(debug=True)
