01e85fe9142f24fdba2e5ca59b531170ad8c4359
msvvy <rakeshkr.kumar290@gmail.com>
2022-03-30 11:54:21 +0530

    Comment 2

--- rakesh2.py
+++ rakesh2.py
@@ -8,7 +8,7 @@ def rakesh2():
 
 @app.route("/")
 def rakesh1():
-    return "Hello Rakesh and jeet welcome to emicon and resourcebusy"
+    return "Hello Rakesh and jeet welcome to emicon"
     
 if __name__=='__main__':
     app.run(debug=True)
